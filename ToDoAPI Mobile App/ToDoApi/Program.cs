2cc360361d3f0298c8cc294dd4e086fe6610f76f
devidhungana <43016940+devidhungana@users.noreply.github.com>
2018-11-23 19:11:21 -0500

    Add files via upload

---
+++ ToDoAPI Mobile App/ToDoApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ToDoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            CreateWebHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
